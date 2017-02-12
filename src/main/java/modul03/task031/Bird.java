70851677e5250ce774ce820c16de8f84f271e186
MYKOLA.GOROKHOV <gorohofff@mail.ru>
2017-02-12 20:49:34 +0200

    Home Work 03

--- src/main/java/modul03/task031/Bird.java
+++ src/main/java/modul03/task031/Bird.java
@@ -11,28 +11,34 @@ package modul03.task031;
  * I am Bird
  */
 public class Bird {
-    final private String STRING = "I am walking\nI am flying\nI am singing\nI am Bird";
+    static final String STRING = "I am walking\nI am flying\nI am singing\nI am Bird";
+    private String name;
 
     public static void main(String[] args) {
 
         System.out.println("Without name");
         Bird bird = new Bird();
         bird.sing();
+        System.out.println();
 
         System.out.println("With name");
         Bird birdVoody = new Bird("Voody");
         birdVoody.sing();
     }
 
-    public Bird(String BirdName) {
-        System.out.println("I am " + BirdName);
-
+    public Bird(String name) {
+        this.name = name;
     }
 
     public Bird() {
     }
 
     public void sing() {
+
+        if (name != null) {
+            System.out.println("I am " + name);
+        }
+
         System.out.println(STRING);
     }
 }
