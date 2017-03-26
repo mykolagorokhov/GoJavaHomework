5786a96e8f56bfd044a4bd451b7f99e95d25ed13
MYKOLA.GOROKHOV <gorohofff@mail.ru>
2017-03-26 21:41:46 +0300

    Module 08

--- src/main/java/modul08/task083/IManageSystem.java
+++ src/main/java/modul08/task083/IManageSystem.java
@@ -24,19 +24,29 @@ public interface IManageSystem<T> {
 
     T save(T obj, double price);
 
-//    T save(T obj);
-//
-//    void delete(T obj);
-//
-//    void deleteById(int id);
-//
-//    T get(int id);
-//
-//    Double getPrice(T obj);
-//
-//    Set<T> getProducts();
-//
-//    List<Double> getPrices();
+    T save(T obj);
 
+    void delete(T obj);
+
+    void deleteById(int id);
+
+    T get(int id);
+
+    Double getPrice(T obj);
+
+    Set<T> getProducts();
+
+    List<Double> getPrices();
+
+//------------------------------------------------------------------------------------------------------
+//    ДЗ №08.6 ** Дополнительно
+//
+//    Добавьте в интерфейс методы и проверьте их работу:
+//    void printProductsSortedByName(); - выводит список «продукт-цена», отсортированных по имени продукта.
+//    void printProductsSortedByPrice();- выводит список «продукт-цена», отсортированных по цене продукта (по возрастанию).
+//------------------------------------------------------------------------------------------------------
+
+//    void printProductsSortedByName();
+//    void printProductsSortedByPrice();
 
 }
